ea12d0376442779dbe9dbb4818ccacb91a25b734
zhangcy <2304417358@qq.com>
2023-01-16 13:08:07 +0800

    C语言督学营 - 6.3 指针的偏移使用场景

---
+++ cmake-build-debug/CMakeFiles/6_3_pointer_deviation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6_3_pointer_deviation.exe"
+  "6_3_pointer_deviation.exe.manifest"
+  "6_3_pointer_deviation.pdb"
+  "CMakeFiles/6_3_pointer_deviation.dir/main.c.obj"
+  "lib6_3_pointer_deviation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/6_3_pointer_deviation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
